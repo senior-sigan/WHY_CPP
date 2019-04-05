34c2ef19733da103076c862e06b6c6503b303a18
Kirill Leyfer <kolbasisha@gmail.com>
2019-04-05 17:33:54 +0300

    Fix various C API definition issues.

--- whycpp_c/include/whycpp_c/input.h
+++ whycpp_c/include/whycpp_c/input.h
@@ -4,10 +4,13 @@
 #include <stdbool.h>
 #include "buttons.h"
 
+WHYCPP_C_API
 bool IsPressed_C(Button_t btn);
 
+WHYCPP_C_API
 bool IsClicked_C(Button_t btn);
 
+WHYCPP_C_API
 void GetMouse_C(int* x, int* y);
 
 #endif //WHYCPP_C_INPUT_H
--- whycpp_c/src/time.cpp
+++ whycpp_c/src/time.cpp
@@ -2,6 +2,7 @@
 // Created by k.leyfer on 28.03.2019.
 //
 
+#include <whycpp_c/time.h>
 #include <whycpp/time.h>
 #include "internal/global_context_int.h"
 
