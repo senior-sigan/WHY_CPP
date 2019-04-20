315ffff5c0e11b9f24f00e6b94e602fbfc02c917
Ilya Siganov <ilya.blan4@gmail.com>
2019-04-21 00:09:55 +0600

    fix picopng header

--- whycpp/lib/lodepng/include/lodepng/picopng.h
+++ whycpp/lib/lodepng/include/lodepng/picopng.h
@@ -2,6 +2,7 @@
 #define WHYCPP_PICOPNG_H
 
 #include <vector>
+#include <cstdlib>
 
 int decodePNG(std::vector<unsigned char>& out_image, unsigned long& image_width, unsigned long& image_height, const unsigned char* in_png, size_t in_size, bool convert_to_rgba32 = true);
 
